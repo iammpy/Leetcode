6e3467301a08e875063d0f24a439919d1e7f6c8a
iammpy <56222228+iammpy@users.noreply.github.com>
2023-04-05 15:06:45 +0800

    Create 19. 删除链表的倒数第 N 个结点.cpp

---
+++ 2023.4/19. 删除链表的倒数第 N 个结点.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode* root=new ListNode(0);
+        root->next=head;
+
+        ListNode* p1=root,*p2=root;
+        for(int i=0;i<n+1;i++){
+            p1=p1->next;
+        }
+
+        while(p1!=nullptr){
+            p1=p1->next;
+            p2=p2->next;
+        }
+        p2->next=p2->next->next;
+
+        return root->next;
+
+    }
+};
