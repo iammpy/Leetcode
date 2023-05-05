0783689daa99eb5aed77ef5f9d6dd26d56ee7357
iammpy <56222228+iammpy@users.noreply.github.com>
2023-05-05 19:55:17 +0800

    Create 剑指 Offer II 036. 后缀表达式.cpp

---
+++ 2023.5/剑指 Offer II 036. 后缀表达式.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int res;
+
+        stack<int> s;
+        for(string str:tokens){
+            if(str[0]=='+'){
+
+            }else if(str[0]=='-'){
+
+            }else if(str[0]=='*'){
+
+            }else if(str[0]=='/'){
+                
+            }else{
+
+            }
+        }
+
+
+
+        return res;
+
+    }
+};
