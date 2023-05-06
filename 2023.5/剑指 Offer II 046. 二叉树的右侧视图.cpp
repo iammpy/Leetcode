a16f56a88affebbf64c9ff0bc40697029751823f
iammpy <56222228+iammpy@users.noreply.github.com>
2023-05-06 13:26:15 +0800

    Create 剑指 Offer II 046. 二叉树的右侧视图.cpp

---
+++ 2023.5/剑指 Offer II 046. 二叉树的右侧视图.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        // 层次遍历，队列记录深度信息，同时保存一个上一个节点的值，当层次更新时，将上一个节点保存到数组中
+
+    }
+};
